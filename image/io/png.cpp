34324759f37840b45811a7de7ff4147fdd299222
Branislav Goga <brano.goga@centrum.sk>
2022-05-16 18:12:38 +0200

    Do not copy data when saving PNG

--- image/io/png.cpp
+++ image/io/png.cpp
@@ -7,16 +7,19 @@
 #include <string>
 
 bool savePng(const std::string& filename, const Image& image) {
+    // TODO: RAII File wrapper
     FILE *fp = fopen(filename.c_str(), "wb");
     if (!fp)
         return false;
 
+    // TODO: RAII PNG wrapper
     png_structp png_ptr = png_create_write_struct
             (PNG_LIBPNG_VER_STRING, nullptr,
              nullptr, nullptr);
     if (!png_ptr)
         return false;
 
+    // TODO: RAII PNG info wrapper
     png_infop info_ptr = png_create_info_struct(png_ptr);
     if (!info_ptr) {
         png_destroy_write_struct(&png_ptr,
@@ -29,7 +32,7 @@ bool savePng(const std::string& filename, const Image& image) {
 
     png_init_io(png_ptr, fp);
 
-// Output is 8bit depth, RGBA format.
+    // Output is 8bit depth, RGBA format.
     png_set_IHDR(
             png_ptr,
             info_ptr,
@@ -48,16 +51,13 @@ bool savePng(const std::string& filename, const Image& image) {
 
     png_bytep *row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * image.getHeight());
     for(size_t y = 0; y < image.getHeight(); y++) {
-        row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png_ptr,info_ptr));
-        memcpy(row_pointers[y], &(pixel_data[image.toIndex(y,0)]), image.getRowSize());
+        row_pointers[y] = const_cast<png_bytep>(pixel_data + y*image.getRowSize());
     };
 
+    // TODO: Allow to define transformation BGRA (GPU) > RGBA ....
     png_write_image(png_ptr, row_pointers);
     png_write_end(png_ptr, NULL);
 
-    for(size_t y = 0; y < image.getHeight(); y++) {
-        free(row_pointers[y]);
-    }
     free(row_pointers);
 
     fclose(fp);
